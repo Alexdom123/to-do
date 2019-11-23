673bb739395a4cd3c9e3e6027c1664da134270de
Alexdom <alexmoreno.am455@gmail.com>
2019-11-23 15:26:58 -0600

    Ya se puede guardar info en la base de datos desde el form de materias

--- main.py
+++ main.py
@@ -1,38 +1,31 @@
-from flask import Flask
-from flask import render_template
-from flask import request
-from flask import make_response
-from flask import session
-from flask import url_for
-from flask import redirect
-
-'''from flaskext.mysql import MySQL
-mysql = MySQL()
-mysql.init_app(app)
-
-cursor = mysql.get_db().cursor()'''
-
+from flask import Flask,render_template, request, make_response, session, url_for, redirect, flash
+from flask_mysqldb import MySQL
 import form
 
 app = Flask(__name__)
-'''app.config["MYSQL_DATABASE_HOST"]
-app.config["MYSQL_DATABASE_PORT"]
-app.config["MYSQL_DATABASE_USER"] = "root"
-app.config["MYSQL_DATABASE_PASSWORD"] = ""
-app.config["MYSQL_DATABASE_DB"] = "to_do"'''
+
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'to_do'
+mysql = MySQL(app)
 
 @app.route('/')
 def index():
   return render_template('index.html')
 
-@app.route('/newmateria')
-def materia():
-  form_list = form.ListaForm()
-  return render_template('newmateria.html', form = form_list)
+@app.route('/main_materia')
+def main_materia():
+  return render_template('main_materia.html')
 
-@app.route('/materiaward')
-def materiaward():
-  return render_template('materiaward.html')
+@app.route('/addmateria',methods=['POST'])
+def addmateria():
+  if request.method == 'POST':
+    materia = request.form['materia']
+    cur = mysql.connection.cursor()
+    cur.execute("INSERT INTO materias (materia) VALUES (%s)", (materia,))
+    mysql.connection.commit()
+    return redirect(url_for('main_materia'))
 
 @app.route('/llenartareas')
 def llenartareas():
